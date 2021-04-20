53548aa216d6ca71bf4ccaf92d42a933a17cf459
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-20 15:11:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,57 @@
-let userId = "tharunece95@gmail.com";
-let url = `https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`;
+let url = `https://gmail.googleapis.com/gmail/v1/users/`;
 let scope = "https://mail.google.com/";
 let client_id =
   "483228225913-7tmuhk2cicdd85nbo4nboaujmn6lr0sk.apps.googleusercontent.com";
 
 let token_url = "https://accounts.google.com/o/oauth2/token";
 
-async function getToken() {
-  let res = await fetch(`https://accounts.google.com/o/oauth2/v2/auth?
-    scope=${scope}&
-    include_granted_scopes=true&
-    response_type=token&
-    state=state_parameter_passthrough_value&
-    redirect_uri=https://tharun-coder.github.io/Gmail_temp/&
-    client_id=${client_id}`,{
-       method:"POST",
-       headers: {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}});
-  let data = res.json();
-  console.log(res);
-}
+function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+//         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//         console.log("Full Name: " + profile.getName());
+//         console.log("Given Name: " + profile.getGivenName());
+//         console.log("Family Name: " + profile.getFamilyName());
+//         console.log("Image URL: " + profile.getImageUrl());
+         let userId = profile.getEmail();
 
-// getToken();
-
+        // The ID token you need to pass to your backend:
+        var access_token = googleUser.getAuthResponse(true).access_token;
+        console.log("Access Token: " + access_token);
+      
+let messages = []
 async function getData() {
   try{
   let res = await fetch(
     url +
-      `/178e9d13d2acc926?access_token=ya29.a0AfH6SMB6NuYpIwZ-Tcv0k3Q1zx1pQ1El_GyJ2D62_JH-vFZ-j34q1xPSdnD9-vqxWNbSMkeEgLdk6qkhRYeAjA1iDg7ntAox_B2zLtzN9qGkn6_OYZ69HcTsbfVVDFjPFtvx4vVXt7BEBkaoGOPSF55QIA78Nw`
+      `${userId}/messages?access_token=${access_token}`
   );
   let data = await res.json();
   console.log(data);
-  let messages = [];
-//   data.messages.forEach((e) => messages.push(getMessages(e.id)));
-//   console.log(messages);
+  
+  data.messages.forEach((e) => getMessages(e.id))
   } catch(err){
     console.log(err)
   }
 }
 
-// getData();
+getData();
 
-async function getMessages() {
-  try{
-  let msg = await fetch(
-    url +
-      `/178e9d13d2acc926?access_token=ya2.a90AfH6SMBfrIKm3EQy0tK7y8HIeErf-uzDI2s3mDC-ADK04Gb99iFsvGB--WcY6iMaDQkV2KKZeKABTSSPFg-YYGqmXlBmZrW2ipo-HeeXgfDhZ_TWDeibkgG99KodQl-spq0go9yKrhJ5DIT0ZZcRufrCZTcg`
-  );
-  let msg_data = await msg.json();
-    console.log(msg_data)
-  return msg_data;
-  } catch(err){
-   console.log(err)
+async function getMessages(id) {
+    try {
+      let msg = await fetch(
+        url + `${userId}/messages/${id}?access_token=${access_token}`
+      );
+      let msg_res = await msg.json();
+      messages.push(msg_res)
+    } catch (err) {
+      return err;
+    }
   }
+ console.log(messages);
 }
 
-// getMessages()
+
 let inbox = [
   {
     id: 1,
